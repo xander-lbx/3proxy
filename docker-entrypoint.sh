30c65a28aa2a8d9e26125af81917fea88e344a80
ErgoZ Riftbit Vaper <ergozru@gmail.com>
2018-09-18 04:27:37 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
 
 if [ "$1" = "start_proxy" ]; then
-if [ ! -f /etc/3proxy/cfg/3proxy.cfg ]; then
-    if [ -z "$PROXY_LOGIN" ] || [ -z "$PROXY_PASSWORD" ]; then
-			echo >&2 'error: proxy is uninitialized, variables is not specified '
-			echo >&2 '  You need to specify PROXY_LOGIN and PROXY_PASSWORD'
-			exit 1
+    if [ ! -f /etc/3proxy/cfg/3proxy.cfg ]; then
+        if [ -z "$PROXY_LOGIN" ] || [ -z "$PROXY_PASSWORD" ]; then
+            echo >&2 'error: proxy is uninitialized, variables is not specified '
+            echo >&2 '  You need to specify PROXY_LOGIN and PROXY_PASSWORD'
+            exit 1
+        fi
+        
+        echo "nserver 1.1.1.1" > /etc/3proxy/cfg/3proxy.cfg
+        echo "nserver 8.8.8.8" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "nserver 8.8.4.4" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "nscache 65536" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "nsrecord www.porno.com 127.0.0.1" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "log" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "logformat "L%t.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "monitor /etc/3proxy/cfg/3proxy.cfg" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "users $PROXY_LOGIN:CL:$PROXY_PASSWORD" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "auth strong" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "proxy -p3128" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "socks -p1080" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "admin -p8080 -w" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "" >> /etc/3proxy/cfg/3proxy.cfg
+        echo "flush" >> /etc/3proxy/cfg/3proxy.cfg
+
+        echo "Proxy user login:    $PROXY_LOGIN"
+        echo "Proxy user password: $PROXY_PASSWORD"
+        echo "Proxy process started!"
+        /etc/3proxy/3proxy /etc/3proxy/cfg/3proxy.cfg
     fi
-	echo "
-nserver 1.1.1.1
-nserver 8.8.8.8
-nserver 8.8.4.4
-nscache 65536
-nsrecord www.porno.com 127.0.0.1
-
-log
-
-logformat "L%t.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"
-
-monitor /etc/3proxy/cfg/3proxy.cfg
-
-users $PROXY_LOGIN:CL:$PROXY_PASSWORD
-
-auth strong
-proxy -p3128
-socks -p1080
-admin -p8080 -w
-
-flush
-" > /etc/3proxy/cfg/3proxy.cfg
-	echo "Proxy user login:         $PROXY_LOGIN"
-	echo "Proxy user password:      $PROXY_PASSWORD"
-	echo "Proxy process started!"
-	/etc/3proxy/3proxy /etc/3proxy/cfg/3proxy.cfg
-fi
 else
 	exec "$@"
 fi
-
-	
